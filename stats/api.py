198a5c2ec5ac1d6cdae7e337b2ce6d63d09d57ac
masonrs2 <masonsepulveda@gmail.com>
2023-11-19 16:36:28 -0600

    Added API that allows a user to specify which team and stat that would like to query for and returns the players from that team in descending order based on the stat paramter passed

--- stats/api.py
+++ stats/api.py
@@ -3,55 +3,82 @@ import pandas as pd
 from ninja import Router
 from nba_api.stats.static import teams
 from nba_api.stats.endpoints import leaguedashplayerstats
-from .constants.constants import keep_columns
+from .constants.constants import keep_columns, all_stats_columns, GetTeamsDict, GetTeamsList
 from .Enums.stats import Stats
+from ninja.errors import HttpError
 
 router = Router()
 
-# TODO: Add a route given a team and a type of stat, we will return the top 3 players on that team for that stat
 # TODO: Add a route given a team and a type of stat, we will return the bottom 3 players on that team for that stat (given the player has played an average of at least 12 minutes per game) 
 
 @router.get("/leadingScorers")
 def leadingScorers(request):
-    return get_player_stats("2023-24", Stats.POINTS_PER_GAME.value)
+    return GetPlayerStats("2023-24", Stats.POINTS_PER_GAME.value)
 
 @router.get("/leadingAssists")
 def leadingAssists(request):
-    return get_player_stats("2023-24", Stats.ASSISTS.value)
+    return GetPlayerStats("2023-24", Stats.ASSISTS.value)
 
 @router.get("/leadingRebounds")
 def leadingRebounds(request):
-    return get_player_stats("2023-24", Stats.REBOUNDS.value)
+    return GetPlayerStats("2023-24", Stats.REBOUNDS.value)
 
 @router.get("/leadingBlocks")
 def leadingBlocks(request):
-    return get_player_stats("2023-24", Stats.BLOCKS.value)
+    return GetPlayerStats("2023-24", Stats.BLOCKS.value)
 
 @router.get("/leadingSteals")
 def leadingSteals(request):
-    return get_player_stats("2023-24", Stats.STEALS.value)
+    return GetPlayerStats("2023-24", Stats.STEALS.value)
 
-@router.get("/team/{team}/leading{stat}")
+@router.get("/{team}/{stat}")
 def leadingStatsByTeam(request, team: str, stat: str):
     try:
-        print("hello")
-    except:
-        print("hello")
+        team = team.lower()
+        if stat.upper() not in all_stats_columns:
+            raise HttpError(400, "Invalid stat parameter provided.")
+        
+        team_dict = GetTeamsDict()
+        matching_team = None
+        matching_key = None
+        if team in team_dict:
+            matching_team = team_dict[team]
+            matching_key = team
+        else:
+            for key, value in team_dict.items():
+                if team in [value['full_name'], value['nickname']]:
+                    matching_team = value
+                    matching_key = key
+                    break
+        
+        if not matching_key:
+            raise HttpError(400, "Invalid team parameter provided.")
+        df = GetPlayerStatsDf("2023-24")
+        df_team = df[df['TEAM_ABBREVIATION'] == matching_key.upper()]
+        df_team = df_team.sort_values(by=[stat], ascending=False)
+        print(df_team)
+        return df_team.to_json(orient='records')
+            
+    except Exception as e:
+        print("An error occurred:", e)
+        raise HttpError(500,"Invalid Query Parameter Passed.")
     return
 
 ## TODO: Need to add routes for all-time leaders for each stat not just for a particular season
 
 ## might need to add a team paramter and if there is a team paramter we need to filter the df by that team and then sort by the stat potentially
-def get_player_stats(season, sort_by):
-    player_stats = leaguedashplayerstats.LeagueDashPlayerStats(season=season)
-
-    df = player_stats.get_data_frames()[0]
-    
-    df = df[keep_columns]
-    df[Stats.POINTS_PER_GAME.value] = df[Stats.POINTS.value] / df[Stats.GAMES_PLAYED.value]
+def GetPlayerStats(season, sort_by):
+    df = GetPlayerStatsDf(season)
 
     # might need to abstract these 2 lines into another function since there may be times where we need to sort by ascending order
     df_sorted = df.sort_values(by=[sort_by], ascending=False)
     sorted_json = df_sorted.to_json(orient='records')
 
     return sorted_json
+
+def GetPlayerStatsDf(season):
+    player_stats = leaguedashplayerstats.LeagueDashPlayerStats(season=season)
+    df = player_stats.get_data_frames()[0]
+    df = df[keep_columns]
+    df[Stats.POINTS_PER_GAME.value] = df[Stats.POINTS.value] / df[Stats.GAMES_PLAYED.value]
+    return df
--- stats/constants/constants.py
+++ stats/constants/constants.py
@@ -7,13 +7,30 @@ keep_columns = [
     'TOV', 'STL', 'BLK', 'BLKA', 'PF', 'PFD', 'PTS', 'PLUS_MINUS','NBA_FANTASY_PTS', 'PLUS_MINUS_RANK', 'NBA_FANTASY_PTS_RANK'
 ] 
 
+all_stats_columns = [
+    'PLAYER_ID', 'PLAYER_NAME', 'NICKNAME', 'TEAM_ID', 'TEAM_ABBREVIATION',
+    'AGE', 'GP', 'W', 'L', 'W_PCT', 'MIN', 'FGM', 'FGA', 'FG_PCT', 'FG3M',
+    'FG3A', 'FG3_PCT', 'FTM', 'FTA', 'FT_PCT', 'OREB', 'DREB', 'REB', 'AST',
+    'TOV', 'STL', 'BLK', 'BLKA', 'PF', 'PFD', 'PTS', 'PLUS_MINUS','NBA_FANTASY_PTS', 'PLUS_MINUS_RANK', 'NBA_FANTASY_PTS_RANK', 'PPG'
+] 
+
 def GetTeamsList():
-    teams = teams.get_teams()
-    teams_info = [item for team in teams for item in [team['full_name'], team['abbreviation'], team['nickname'], team['city']]]
+    teams_data = teams.get_teams()
+    teams_info = [item for team in teams_data for item in [team['full_name'], team['abbreviation'], team['nickname'], team['city']]]
     return teams_info
 
 def GetTeamsDict():
-    teams = teams.get_teams()
-    team_dict = {team['abbreviation']: {'id': team['id'], 'full_name': team['full_name'], 'nickname': team['nickname'], 'city': team['city'], 'state': team['state'], 'year_founded': team['year_founded']} for team in teams}
+    teams_data = teams.get_teams()
+    team_dict = {
+        team['abbreviation'].lower(): 
+             {
+                'id': team['id'], 
+                'full_name': team['full_name'].lower(), 
+                'nickname': team['nickname'].lower(), 
+                'city': team['city'].lower(), 
+                'state': team['state'].lower(), 
+                'year_founded': team['year_founded']
+              } 
+    for team in teams_data
+    }
     return team_dict
-
