7c93a31538cd0d60aa334adf2d63aaf35f374e23
masonrs2 <masonsepulveda@gmail.com>
2023-11-19 21:12:26 -0600

    Added endpoint to get the results of every game for a given team in a given season depending on user input

--- stats/api.py
+++ stats/api.py
@@ -1,8 +1,9 @@
 import json
 import pandas as pd
+from typing import Optional
 from ninja import Router
 from nba_api.stats.static import teams
-from nba_api.stats.endpoints import leaguedashplayerstats
+from nba_api.stats.endpoints import leaguedashplayerstats, leaguegamefinder
 from .constants.constants import keep_columns, all_stats_columns, GetTeamsDict, GetTeamsList
 from .Enums.stats import Stats
 from ninja.errors import HttpError
@@ -11,6 +12,8 @@ router = Router()
 
 # TODO: Add a route given a team and a type of stat, we will return the bottom 3 players on that team for that stat (given the player has played an average of at least 12 minutes per game) 
 
+
+## Should add the season parameter in all routes here instead of hard coding
 @router.get("/leadingScorers")
 def leadingScorers(request):
     return GetPlayerStats("2023-24", Stats.POINTS_PER_GAME.value)
@@ -53,6 +56,7 @@ def leadingStatsByTeam(request, team: str, stat: str):
         
         if not matching_key:
             raise HttpError(400, "Invalid team parameter provided.")
+        ## Should add the season parameter here instead of hard coding
         df = GetPlayerStatsDf("2023-24")
         df_team = df[df['TEAM_ABBREVIATION'] == matching_key.upper()]
         df_team = df_team.sort_values(by=[stat], ascending=False)
@@ -62,7 +66,38 @@ def leadingStatsByTeam(request, team: str, stat: str):
     except Exception as e:
         print("An error occurred:", e)
         raise HttpError(500,"Invalid Query Parameter Passed.")
-    return
+
+@router.get("/schedule")
+def GetGameResultsByTeam(request, team: str, season: Optional[str] = None):
+    try:
+        team = team.lower()
+        team_dict = GetTeamsDict()
+        matching_team = None
+        matching_key = None
+        if team in team_dict:
+            matching_team = team_dict[team]
+            matching_key = team
+        else:
+            for key, value in team_dict.items():
+                if team in [value['full_name'], value['nickname']]:
+                    matching_team = value
+                    matching_key = key
+                    break
+        
+        if not matching_key:
+            raise HttpError(400, "Invalid team parameter provided.")
+       
+        print("matching key: ", matching_key)
+        gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=matching_team['id'])
+        games = gamefinder.get_data_frames()[0]
+        games = games[games.SEASON_ID.str[-4:] == "2023"]
+        return games.to_json(orient='records')
+        
+    except Exception as e:
+        print("An error occurred:", e)
+        raise HttpError(500,"Invalid Query Parameter Passed.")
+
+## TODO: Get the W/L history for a specified team.
 
 ## TODO: Need to add routes for all-time leaders for each stat not just for a particular season
 
