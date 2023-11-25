b586f25d8047277f1caf1db0addd8deb8307f04e
masonrs2 <masonsepulveda@gmail.com>
2023-11-25 16:48:01 -0600

    Implemented routing for Complete player stat table

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,14 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import CompleteStatCategories from "./components/CompleteStatCategories"
 import Navbar from "./components/Navbar"
 import { StatsLeadingCards } from "./components/StatsLeadingCards"
+import PlayerStatsTable from "./components/PlayerStatsTable"
 
 function App() {
 
   return (
     <div className='bg-zinc-800 h-full w-screen'>
       <Navbar />
-      <CompleteStatCategories />
-      <StatsLeadingCards />
+        <Routes>
+          <Route exact path="/nba/stats/players/:stat" component={PlayerStatsTable} />
+          <Route path="/" element={
+            <>
+              <CompleteStatCategories />
+              <StatsLeadingCards />
+            </>
+          }>
+          </Route>
+        </Routes>
     </div>
   )
 }
