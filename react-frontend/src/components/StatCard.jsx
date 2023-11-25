287c69053cdd3a80ff71c19b1ac5eff471d85f5f
masonrs2 <masonsepulveda@gmail.com>
2023-11-24 21:44:06 -0600

    Finished leading{stat} preview tables

--- react-frontend/src/components/StatCard.jsx
+++ react-frontend/src/components/StatCard.jsx
@@ -8,12 +8,13 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { TeamLogos, getTeamLogo } from "../assets/constants/TeamLogos";
+import { TfiUser } from "react-icons/tfi"
 
 const StatCard = ({ key, stat }) => {
     const [playerData, setPlayerData] = useState([]);
 
     const fetchData = (stat) => {
-      fetch(`http://127.0.0.1:8000/api/leading${stat}?season=2012-13`)
+      fetch(`http://127.0.0.1:8000/api/leading${stat}?season=2023-24`)
         .then(response => {
           if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
@@ -41,7 +42,44 @@ const StatCard = ({ key, stat }) => {
 
   return (
     <div className="flex flex-col text-zinc-400 font-medium bg-zinc-900 outline outline-[.5px] outline-gray-500 m-4">
-      <h1 className="p-3 px-4 flex outline outline-[.5px] outline-gray-500">{stat?.Stat} Per Game</h1>
+      <h1 className="p-3 px-5 flex outline outline-[.5px] outline-gray-500">{stat?.Stat} Per Game</h1>
+      <div className="outline outline-gray-500 outline-[.5px]">
+        <Table className="">  
+            <TableBody>
+                <TableRow className="cursor-pointer">
+                <TableCell>
+                    <p className="text-2xl font-semibold">1</p>
+                    </TableCell>
+                    <TableCell>
+                    <div className="flex items-center gap-1">
+                        <div className="w-8 h-8 rounded-full overflow-hidden flex items-center">
+                        <img className="object-cover" src={getTeamLogo(playerData[0]?.TEAM_ABBREVIATION)} alt={playerData[0]?.TEAM_ABBREVIATION} />
+                        </div>
+                        <div className="rounded-full overflow-hidden flex items-center">
+                        <TfiUser height={20} width={20} className="object-cover h-9 w-9" src={getTeamLogo(playerData[0]?.TEAM_ABBREVIATION)} alt={playerData[0]?.TEAM_ABBREVIATION} />
+                        </div>
+                    </div>
+                    </TableCell>
+                <TableCell>
+                    <div className="flex flex-col px-2">
+                    <p className="text-lg">{playerData[0]?.PLAYER_NAME}</p>
+                    <p className="font-light">{playerData[0]?.TEAM_ABBREVIATION} • ({playerData[0]?.AGE})</p>
+                    </div>
+                </TableCell>
+                <TableCell>
+                    <div className="flex flex-col px-2 text-2xl items-center text-right">
+                    {playerData.length > 0 && playerData[0][stat.Abbreviation] && 
+                        <p className="text-right w-full">
+                        {stat.Abbreviation === 'PPG' ? playerData[0][stat.Abbreviation].toFixed(1) : (playerData[0][stat.Abbreviation] / playerData[0]["GP"]).toFixed(1)}
+                        </p>
+                    }
+                    <p className="font-light text-xs text-right w-full">{stat?.Abbreviation.slice(0,1)}PG</p>
+                    </div>
+                </TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
+      </div>
       <div>
         <Table>
           <TableHeader>
@@ -55,15 +93,20 @@ const StatCard = ({ key, stat }) => {
           <TableBody>
             {
               Array.isArray(playerData) &&  playerData.slice(1,6).map((player, index) => (
-                <TableRow key={index+1}>
-                  <TableCell>{index+2}</TableCell>
-                  <TableCell className="flex gap-4 items-center ">
-                    <div className="w-8 h-8 rounded-full overflow-hidden flex items-center">
-                        <img className="object-cover" src={getTeamLogo(player?.TEAM_ABBREVIATION)} alt={player?.TEAM_ABBREVIATION} />
-                    </div>{player?.TEAM_ABBREVIATION}
-                </TableCell>
-                  <TableCell>{player?.PLAYER_NAME}</TableCell>
-                  <TableCell>{player[stat.Abbreviation]}</TableCell>
+                <TableRow className="cursor-pointer" key={index+1}>
+                    <TableCell className="font-semibold">{index+2}</TableCell>
+                    <TableCell className="flex gap-4 items-center ">
+                        <div className="w-8 h-8 rounded-full overflow-hidden flex items-center">
+                            <img className="object-cover" src={getTeamLogo(player?.TEAM_ABBREVIATION)} alt={player?.TEAM_ABBREVIATION} />
+                        </div>{player?.TEAM_ABBREVIATION}
+                    </TableCell>
+                    <TableCell>{player?.PLAYER_NAME}</TableCell>
+                    <TableCell>
+                        {stat.Abbreviation === "PPG" 
+                            ? player[stat.Abbreviation].toFixed(1) 
+                            : (player[stat.Abbreviation] / player?.GP).toFixed(1)
+                        }
+                    </TableCell>
                 </TableRow>
               ))
             }
